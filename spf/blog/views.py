cdc80eb878b43fe792a955b3d52685f93c47e9bd
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-10-09 16:18:56 -0700

    pagination seems to be working along with routing by tag. need to focus on more usability and front-end

--- spf/blog/views.py
+++ spf/blog/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpResponse
 from .models import Article, Tag, ArticleTag, Photo
 
 
 # Create your views here.
 def index(request):
-    articles = Article.objects.all()
-    tags = Tag.objects.all()
+    articles_list = Article.objects.all()
+    page = request.GET.get('page',1)
+
+    paginator = Paginator(articles_list, 1)
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+
     return render(request, 'article_list.html', 
-        {'articles': articles},
-        {'tags': tags})
+        {'articles': articles})
 
 def article_detail(request, pk):
     article = Article.objects.get(id=pk)
@@ -25,3 +34,33 @@ def tag_index(request, pk):
     {'tags':tags},
     {'articles' : articles})
     
+
+# from django.contrib.auth.models import User
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+# def index(request):
+#     user_list = User.objects.all()
+#     page = request.GET.get('page', 1)
+
+#     paginator = Paginator(user_list, 10)
+#     try:
+#         users = paginator.page(page)
+#     except PageNotAnInteger:
+#         users = paginator.page(1)
+#     except EmptyPage:
+#         users = paginator.page(paginator.num_pages)
+
+#     return render(request, 'core/user_list.html', { 'users': users })
+
+
+
+
+
+# In case of screw up - this is the original index view
+# def index(request):
+#     articles = Article.objects.all()
+#     tags = Tag.objects.all()
+    
+#     return render(request, 'article_list.html', 
+#         {'articles': articles},
+#         {'tags': tags})
