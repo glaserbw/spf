442688f9725e880fd6e5f4904688ff8a8738192e
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-10-11 11:30:20 -0700

    CSS updates. Focusing on Admin page for further usability

--- spf/blog/admin.py
+++ spf/blog/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Article
-from .models import Tag
-from .models import Photo
-from .models import ArticleTag
+from .models import Article, Tag, Photo, ArticleTag
+
 
 # Register your models here.
 admin.site.register(Article)
